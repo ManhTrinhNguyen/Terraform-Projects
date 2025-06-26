55e08c331c7ab0f84a262394543f7f3700bd8a2b
Tim <nguyenmanhtrinh17041998@gmail.com>
2025-06-25 19:09:45 -0700

    testttt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,15 @@
-variable "cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "availability_zone" {}
-variable "my_ip_address" {}
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
+variable "subnet_cidr_block" {
+  default = "10.0.0.0/24"
+}
+variable "availability_zone" {
+  default = "us-west-1a"
+}
+variable "my_ip_address" {
+  default = "157.131.152.31/32"
+}
 variable "instance_type" {}
 variable "env_prefix" {}
 variable "my_key_name" {}
